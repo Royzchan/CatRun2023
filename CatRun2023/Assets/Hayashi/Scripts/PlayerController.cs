ae9f461bda15c40e410325452db621ec216a024d
林　利樹 <117792737+Royzchan@users.noreply.github.com>
2023-12-08 12:03:04 +0900

    AddModelsAndPlayerMove

--- CatRun2023/Assets/Hayashi/Scripts/PlayerController.cs
+++ CatRun2023/Assets/Hayashi/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Header("プレイヤーの移動速度")]
     private float _runSpeed = 5.0f;
 
+    //走り出した時のスピード
     private float _startRunSpeed;
 
     [SerializeField, Header("プレイヤーのジャンプの速さ")]
@@ -22,11 +24,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Header("プレイヤーがジャンプできる高さ")]
     private float _canJumpHeight = 3.0f;
 
+    [SerializeField, Header("ノックバックする強さ")]
+    private float _knockbackPower = 5.0f;
+
+    //ノックバックするかどうか
+    private bool _canKnockback = false; 
+
+    //現在ノックバック中かどうか
+    private bool _knockbackNow = false;
+
     //ジャンプの時に使う高さを図る為の変数
     private float _jumpStartPos = 0f;
-
-    //浮けるかどうか
-    private bool _canFloat = false;
 
     //ジャンプした回数(カウント用)
     private int _jumpNum = 0;
@@ -41,8 +49,34 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //移動速度を走っているスピードにする
-        rb.velocity = new Vector3(_runSpeed, rb.velocity.y, 0f);
+
+        //現在の座標から前にレイを飛ばす飛ばす
+        //今回は向いている方向の都合上、フォワードにマイナスをかける
+        Ray Forwardray = new Ray(this.transform.position - new Vector3(0f, 0.3f, 0f), -this.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(Forwardray, out hit, 1.0f))
+        {
+            //レイの当たったオブジェクトが木だった場合
+            if(hit.collider.gameObject.CompareTag("Wood"))
+            {
+                //ノックバックできる判定をtrueにする
+                _canKnockback = true;
+            }
+        }
+        else
+        {
+            //レイに何も当たっていなかったらノックバックする判定をfalseにする
+            _canKnockback = false;
+        }
+
+
+        //Debug.DrawRay(Forwardray.origin, Forwardray.direction , Color.red, 0.5f);
+        //ノックバック中じゃなかった場合
+        if (!_knockbackNow)
+        {
+            //移動速度を走っているスピードにする
+            rb.velocity = new Vector3(_runSpeed, rb.velocity.y, 0f);
+        }
 
 
         //ジャンプのボタンが押されたら
@@ -51,14 +85,15 @@ public class PlayerController : MonoBehaviour
             //ジャンプした回数がジャンプできる回数より低かった時
             if(_jumpNum<_canJumpNum)
             {
+                //二段ジャンプの前にかかっている力を消す
                 if (_jumpNum > 0)
                 {
                     rb.velocity = Vector3.zero;
                 }
                 //ジャンプの回数を足して
                 _jumpNum++;
+                //上に力を加える
                 rb.AddForce(new Vector3(0f, _canJumpHeight, 0f),ForceMode.Impulse);
-                
             }
         }
     }
@@ -69,6 +104,22 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.CompareTag("Ground"))
         {
             _jumpNum = 0;
+            _knockbackNow = false;
+        }
+
+        //木に触れたら
+        if (collision.gameObject.CompareTag("Wood"))
+        {
+            //ノックバックできる判定がtrue(正面に木がある状態)
+            if(_canKnockback)
+            {
+                //ノックバックしている判定をtrueにする
+                _knockbackNow = true;
+                //かかっている力を消す
+                rb.velocity = Vector3.zero;
+                //後ろと上に力を加える
+                rb.AddForce(new Vector3(-_knockbackPower, 3f, 0f), ForceMode.Impulse);
+            }
         }
     }
 }
